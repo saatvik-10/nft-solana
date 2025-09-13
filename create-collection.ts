33036d5fe548052e69ffab98cb801873c73fe9f8
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-14 01:09:55 +0530

    creating nft

---
+++ create-collection.ts
@@ -0,0 +1,60 @@
+//nfts are unique....hv their own mint address. Collection binds them together (nfts of a particular owner)
+import {
+  createNft,
+  fetchDigitalAsset,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const user = await getKeypairFromFile();
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL,
+);
+
+console.log("User loaded", user.publicKey.toBase58());
+
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata()); //wants to talk to the mplTokenMetadata
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("Setting up Umi instance of user");
+
+const collectionMint = generateSigner(umi);
+
+const transaction = await createNft(umi, {
+  mint: collectionMint,
+  name: "SV Collection",
+  symbol: "SVM",
+  uri: "https://raw.githubusercontent.com/saatvik-10/portfolio/main/portfolio/nftMetadata.json",
+  sellerFeeBasisPoints: percentAmount(0),
+  isCollection: true,
+});
+
+await transaction.sendAndConfirm(umi);
+
+const createdCollectionNft = await fetchDigitalAsset(
+  umi,
+  collectionMint.publicKey,
+);
+
+console.log(
+  `Collection created at ${getExplorerLink("address", createdCollectionNft.mint.publicKey, "devnet")}`,
+);
